ef566542e098bfb1287000c514c3014300c60857
kakegawa.atsushi <pasapasa.va1104@gmail.com>
2012-05-22 23:41:31 +0900

    Initial Commit

---
+++ BluetoothSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BluetoothSample
+//
+//  Created by 敦史 掛川 on 12/05/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BluetoothSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BluetoothSample
+//
+//  Created by 敦史 掛川 on 12/05/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
